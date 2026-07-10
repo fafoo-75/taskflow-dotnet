1138be46641b763327ca7c0d62258e86fdafa1e5
fafoo-75 <chalalfarid@gmail.com>
2026-07-10 21:07:56 +0200

    fix: migration AddEstimatedHour

---
+++ Migrations/Sqlite/20260710190710_AddEstimatedHour.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlow.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class AddEstimatedHour : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
